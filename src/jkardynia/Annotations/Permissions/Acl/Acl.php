8e9506a262c450909ee0634bc9da31946a9b51fb
Jarek <jjarekk@gmial.com>
2013-08-18 14:09:59 +0200

    Preparing environment. Creating Acl annotation class.

---
+++ src/jkardynia/Annotations/Permissions/Acl/Acl.php
@@ -0,0 +1,40 @@
+<?php
+namespace Jkardynia\Annotations\Permissions\Acl;
+
+/**
+ * Acl annotation
+ *
+ * @author Jarosław Kardynia
+ */
+class Acl {
+    
+    const ALLOW_ACCESS_TYPE_NAME = "Allow";
+    const DENY_ACCESS_TYPE_NAME = "Deny";
+    
+    private $type = "";
+    
+    public function __construct($params){
+        
+        $this->checkForRequiredParameters($params);
+        $this->checkIfParamsAreCorrect($params);
+        
+        $this->type = $params["value"];
+    }
+    
+    private function checkForRequiredParameters($params){
+        if(false === isset($params["value"]) || 
+                false === isset($params["roles"])){
+            throw new \InvalidArgumentException("There is missing some required params.");
+        }
+    }
+    
+    private function checkIfParamsAreCorrect($params){
+        $type = $params["value"];
+        
+        if(false === in_array($type, array(self::ALLOW_ACCESS_TYPE_NAME, 
+            self::DENY_ACCESS_TYPE_NAME))){
+            throw new \InvalidArgumentException("Wrong ACL type.");
+        }
+    }
+}
+
---
+++ tests/jkardynia/Annotations/Permissions/Acl/AclTest.php
@@ -0,0 +1,38 @@
+<?php
+namespace Jkardynia\Annotations\Permissions\Acl;
+
+require_once '\vendor\autoload.php';
+use \Jkardynia\Annotations\Permissions\Acl\Acl;
+
+class AclTest extends \PHPUnit_Framework_TestCase{
+    
+    /** @test */
+    public function initializeAnnotationWithCorrectParams(){
+        $aclAnnotation = new Acl(array("value" => "Allow",
+            "roles" => "guest"));
+        $this->assertNotNull($aclAnnotation);
+    }
+    
+    /** 
+     * @test
+     * @expectedException \InvalidArgumentException
+     */
+    public function initializeAnnotationWithWrongParamsRisesException(){
+        $aclAnnotation = new Acl(array("val" => "Allow"));
+    }
+    
+    /** @test */
+    public function annotationTypeIsCorrect(){
+        $allowAnnotation = new Acl(array("value" => "Allow", "roles" => "guest"));
+        $denyAnnotation = new Acl(array("value" => "Deny", "roles" => "guest"));
+    }
+    
+    /** 
+     * @test
+     * @expectedException \InvalidArgumentException
+     */
+    public function wrongAnnotationTypeRisesException(){
+        $allowAnnotation = new Acl(array("value" => "WrongType", "roles" => "guest"));
+    }
+}
+
